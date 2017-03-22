15e531eb6805d84298dc029b384d73ae85913f80
Ghosy <zacharymatt5@gmail.com>
2017-03-22 14:49:20 -0400

    Separate entry to own file

--- settings.cpp
+++ settings.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 
+#include "entry.h"
 #include "settings.h"
 
 config settings;
@@ -219,63 +220,6 @@ std::pair<std::vector<unsigned char>, std::vector<unsigned char>> config::read_n
 		}
 	}
 	return make_pair(lows, highs);
-}
-
-Entry::Entry() {
-}
-
-Entry::Entry(std::vector<unsigned char> lows, std::vector<unsigned char> highs, std::string new_action) {
-	for(int i = 0; i < 3; ++i) {
-		min[i] = lows[i];
-		max[i] = highs[i];
-	}
-	action = new_action;
-	light_mode = LightMode::NONE;
-	light_value = 0;
-}
-
-Entry::Entry(std::vector<unsigned char> lows, std::vector<unsigned char> highs, std::string new_action, LightMode new_mode, unsigned char new_light_value) {
-	for(int i = 0; i < 3; ++i) {
-		min[i] = lows[i];
-		max[i] = highs[i];
-	}
-
-	action = new_action;
-	light_mode = new_mode;
-	light_value = new_light_value;
-}
-
-bool Entry::contains(const std::vector<unsigned char>& note) const {
-	for(int i = 0; i < 3; ++i) {
-		if(note[i] < min[i] || note[i] > max[i])
-			return false;
-	}
-	return true;
-}
-
-bool Entry::operator<(const Entry& other) const {
-	for(int i = 0; i < 3; ++i) {
-		if(min[i] < other.min[i] && max[i] < other.max[i])
-			return true;
-	}
-	return false;
-}
-
-std::string Entry::get_note() const {
-	std::ostringstream ostream;
-
-	for(int i = 0; i < 3; ++i) {
-		if(min[i] == max[i]) {
-			ostream << (int)min[i];
-		}
-		else {
-			ostream << (int)min[i] << ".." << (int)max[i];
-		}
-		if(i < 2) {
-			ostream << ",";
-		}
-	}
-	return ostream.str();
 }
 
 // I swear to god if these names aren't fix to be
