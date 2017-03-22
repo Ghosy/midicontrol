ac43ee5d65e80e82d8c6ce1440c6ea843e566a8c
Ghosy <zacharymatt5@gmail.com>
2017-03-22 14:49:03 -0400

    Add entry

---
+++ entry.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include "entry.h"
+Entry::Entry() {
+}
+
+Entry::Entry(std::vector<unsigned char> lows, std::vector<unsigned char> highs, std::string new_action) {
+	for(int i = 0; i < 3; ++i) {
+		min[i] = lows[i];
+		max[i] = highs[i];
+	}
+	action = new_action;
+	light_mode = LightMode::NONE;
+	light_value = 0;
+}
+
+Entry::Entry(std::vector<unsigned char> lows, std::vector<unsigned char> highs, std::string new_action, LightMode new_mode, unsigned char new_light_value) {
+	for(int i = 0; i < 3; ++i) {
+		min[i] = lows[i];
+		max[i] = highs[i];
+	}
+
+	action = new_action;
+	light_mode = new_mode;
+	light_value = new_light_value;
+}
+
+bool Entry::contains(const std::vector<unsigned char>& note) const {
+	for(int i = 0; i < 3; ++i) {
+		if(note[i] < min[i] || note[i] > max[i])
+			return false;
+	}
+	return true;
+}
+
+bool Entry::operator<(const Entry& other) const {
+	for(int i = 0; i < 3; ++i) {
+		if(min[i] < other.min[i] && max[i] < other.max[i])
+			return true;
+	}
+	return false;
+}
+
+std::string Entry::get_note() const {
+	std::ostringstream ostream;
+
+	for(int i = 0; i < 3; ++i) {
+		if(min[i] == max[i]) {
+			ostream << (int)min[i];
+		}
+		else {
+			ostream << (int)min[i] << ".." << (int)max[i];
+		}
+		if(i < 2) {
+			ostream << ",";
+		}
+	}
+	return ostream.str();
+}
---
+++ entry.h
@@ -0,0 +1,30 @@
+#ifndef ENTRY_H_
+#define ENTRY_H_
+
+enum class LightMode {
+	NONE,
+	LIGHT_PUSH,
+	LIGHT_ON,
+	LIGHT_OFF,
+	LIGHT_WAIT
+};
+
+class Entry
+{
+	public:
+		Entry();
+		Entry(std::vector<unsigned char> lows, std::vector<unsigned char> highs, std::string new_action);
+		Entry(std::vector<unsigned char> lows, std::vector<unsigned char> highs, std::string new_action, LightMode new_mode, unsigned char new_light_value);
+		bool contains(const std::vector<unsigned char>& note) const;
+		bool operator<(const Entry& other) const;
+		std::string get_note() const;
+
+		std::string action;
+		LightMode light_mode;
+		unsigned char light_value;
+		// TODO: Should this really be stored as array or use more useful storage
+		unsigned char min[3];
+		unsigned char max[3];
+};
+
+#endif
