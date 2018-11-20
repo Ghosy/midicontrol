fd618d45be4207adb09b44e6a145fb080e69bd5b
Ghosy <zacharymatt5@gmail.com>
2018-11-20 18:55:10 +0000

    Fix unit tests to work with spdlog

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,2 +1,13 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include<catch2/catch.hpp>
+#include<spdlog/sinks/stdout_color_sinks.h>
+
+int main(int argc, char* argv[]) {
+	// Lie that there is a logger
+	auto null_sink = spdlog::stdout_color_mt("multi_sink");
+	null_sink->set_level(spdlog::level::off);
+
+	int result = Catch::Session().run(argc, argv);
+
+	return result;
+}
